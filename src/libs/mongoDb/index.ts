a71a8e81f89ab4b0593dc7ca74810802c0b859eb
Kadir Alp Çil <kadiralpcil@gmail.com>
2024-04-22 15:04:49 +0300

    changed mongodb+uri kadiralpcil to vercel-admin

--- src/libs/mongoDb/index.ts
+++ src/libs/mongoDb/index.ts
@@ -4,7 +4,7 @@ export const connectMongoDB = async () => {
   try {
     await mongoose.connect(
       process.env.MONGODB_URI ??
-        "mongodb+srv://kadiralpcil:szNLzMBrIGazeCdu@cluster0.refdcuw.mongodb.net/todoApp",
+        "mongodb+srv://vercel-admin-user:5dYVtXaqaCrP9Vmz@cluster0.refdcuw.mongodb.net/todoApp",
     );
     console.log("connected mongo");
   } catch (error) {
