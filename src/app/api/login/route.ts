7b5a3db3b84f06563f3f3d29cfcbf3e31e5a0e50
Kadir Alp Çil <kadiralpcil@gmail.com>
2024-05-01 22:50:59 +0300

    response to error messege

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -60,7 +60,7 @@ export async function POST(request: NextRequest) {
   } catch (error) {
     console.error("Hata:", error);
     return NextResponse.json(
-      { success: false, message: "Sunucu hatası" },
+      { success: false, message: error },
       { status: 500 },
     );
   }
