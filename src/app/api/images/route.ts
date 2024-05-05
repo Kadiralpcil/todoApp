00f25a25e918e7653f9621eca8009a22b5091bde
Kadir Alp Çil <kadiralpcil@gmail.com>
2024-05-06 00:04:25 +0300

    request error added to response

--- src/app/api/images/route.ts
+++ src/app/api/images/route.ts
@@ -3,24 +3,23 @@ import path from "path";
 import { writeFile } from "fs/promises";
 
 export const POST = async (req: NextRequest, res: NextResponse) => {
+  const formData = await req.formData();
+  const file = formData.get("file") as File;
+  if (!file) {
+    return NextResponse.json({ error: "No files received." }, { status: 400 });
+  }
 
-    const formData = await req.formData();
-    const file = formData.get("file") as File;
-    if (!file) {
-        return NextResponse.json({ error: "No files received." }, { status: 400 });
-    }
-
-    const buffer = Buffer.from(await file.arrayBuffer());
-    const filename = file.name.replaceAll(" ", "_");
-    console.log(filename);
-    try {
-        await writeFile(
-            path.join(process.cwd(), "public/images/" + filename),
-            buffer
-        );
-        return NextResponse.json({ Message: "Success", status: 201 });
-    } catch (error) {
-        console.log("Error occured ", error);
-        return NextResponse.json({ Message: "Failed", status: 500 });
-    }
+  const buffer = Buffer.from(await file.arrayBuffer());
+  const filename = file.name.replaceAll(" ", "_");
+  console.log(filename);
+  try {
+    await writeFile(
+      path.join(process.cwd(), "public/images/" + filename),
+      buffer,
+    );
+    return NextResponse.json({ Message: "Success", status: 201 });
+  } catch (error) {
+    console.log("Error occured ", error);
+    return NextResponse.json({ Message: error, status: 500 });
+  }
 };
