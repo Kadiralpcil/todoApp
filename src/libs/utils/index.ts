7ef01626b4d03067dd240b3495ce53919b27815e
Kadir Alp Çil <kadiralpcil@gmail.com>
2024-05-05 23:37:49 +0300

    edit apiUrl

--- src/libs/utils/index.ts
+++ src/libs/utils/index.ts
@@ -1,6 +1,8 @@
 import { isDevelopment, isProduction } from "./env";
 
 const apiUrl =
-  process.env.NODE_ENV === "development" ? "devServerURL" : "prodServerURL";
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000/"
+    : "https://todo-app-nine-dusky-49.vercel.app/";
 
 export { isDevelopment, isProduction, apiUrl };
