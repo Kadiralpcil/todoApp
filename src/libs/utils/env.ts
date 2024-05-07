8eec98fdbae68248bb8f8a4681ef9e254041f927
Kadir Alp Çil <kadiralpcil@gmail.com>
2024-05-07 09:49:29 +0300

    removed unused folders

--- src/libs/utils/env.ts
+++
@@ -1,9 +0,0 @@
-const isProduction = () => {
-  return process.env.NODE_ENV ? process.env.NODE_ENV === "production" : false;
-};
-
-const isDevelopment = () => {
-  return !process.env.NODE_ENV || process.env.NODE_ENV === "development";
-};
-
-export { isProduction, isDevelopment };
--- src/libs/utils/index.ts
+++
@@ -1,8 +0,0 @@
-import { isDevelopment, isProduction } from "./env";
-
-const apiUrl =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3000/"
-    : "https://todo-app-nine-dusky-49.vercel.app/";
-
-export { isDevelopment, isProduction, apiUrl };
